8300df209078f83851fd6c6e08ca6eadcb809bdd
loanxinhiu <ntloan21122004@gmail.com>
2025-07-01 19:27:34 +0700

    create function countVietnameseAccentedLetters

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("--Chương trình đếm số lượng các chữ cái có dấu Tiếng Việt có thể được tạo thành trong 1 chuỗi chữ cái latin nhập vào từ bàn phím--");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập chuỗi:");
+        String text = "";
+        do {
+            text = sc.nextLine().toLowerCase();
+            if (!isValid(text)) {
+                System.out.println("Nhập lại chuỗi (Chỉ được chứa chữ cái và chữ số):");
+            }
+        } while (!isValid(text));
+        System.out.println("Số ký tự có dấu có thể tạo thành: " + countVietnameseAccentedLetters(text));
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    static int countVietnameseAccentedLetters(String text) {
+        int count = 0;
+        int length = text.length();
+        for (int i = 0; i < length; i++) {
+            if (i < length - 1) {
+                String pair = text.substring(i, i + 2);
+                switch (pair) {
+                    case "aw":
+                    case "aa":
+                    case "dd":
+                    case "ee":
+                    case "oo":
+                    case "ow":
+                        count++;
+                        i++;
+                        continue;
+                }
+            }
+            if (text.charAt(i) == 'w') {
+                count++;
+            }
         }
+        return count;
+    }
+
+    static boolean isValid(String input) {
+        return input.matches("[a-zA-Z0-9]+");
     }
 }
